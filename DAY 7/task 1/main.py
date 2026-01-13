5bbb4a02b26d3e5ea7228ce524365d219bf00ac3
rohanraj2003 <rohanrajsr2003@gmail.com>
2026-01-13 16:33:31 +0530

    day 7

---
+++ DAY 7/task 1/main.py
@@ -0,0 +1,13 @@
+import math_utils
+
+result1=math_utils.add(10,10)
+
+result2=math_utils.substact(20,11)
+
+result3=math_utils.multiply(2,8)
+
+
+print("Addition",result1)
+print("Addition",result2)
+print("Addition",result3)
+
